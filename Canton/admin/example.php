db3f48725da1ecd56ecf7c2fc6fd7bb7b02412ae
John Louie Navales <104504427+johnlouie09@users.noreply.github.com>
2024-02-02 12:44:04 +0800

    remove Canton folder

--- Canton/admin/example.php
+++
@@ -1,9 +0,0 @@
-<!-- Content Section -->
-<div class="wrapper-content">
-  <div class="center">
-    <h5>Welcome to My Page</h5>
-    <div class="buttons">
-      <button>Log in</button>
-
-    </div>
-  </div>
